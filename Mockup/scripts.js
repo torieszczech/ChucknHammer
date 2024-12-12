5fe36b819f9fc49b82354e421e0dc3ad8d5c6106
torieszczech <SZCZECVM5518@uwec.edu>
2024-12-12 10:13:29 -0600

    12/12 class

---
+++ Mockup/scripts.js
@@ -0,0 +1,42 @@
+//Roof Section
+document.addEventListener("DOMContentLoaded", function(){
+	document.querySelector('.roof>.accordion').addEventListener('click', function(){
+		document.querySelector('.roof>div').classList.toggle('show');
+		document.querySelector('.roof span').classList.toggle('rotate');
+	});
+});
+//Land Section
+document.addEventListener("DOMContentLoaded", function(){
+	document.querySelector('.land>.accordion').addEventListener('click', function(){
+		document.querySelector('.land>div').classList.toggle('show');
+		document.querySelector('.land span').classList.toggle('rotate');
+	});
+});
+//Side Section
+document.addEventListener("DOMContentLoaded", function(){
+	document.querySelector('.side>.accordion').addEventListener('click', function(){
+		document.querySelector('.side>div').classList.toggle('show');
+		document.querySelector('.side span').classList.toggle('rotate');
+	});
+});
+//Window Section
+document.addEventListener("DOMContentLoaded", function(){
+	document.querySelector('.window>.accordion').addEventListener('click', function(){
+		document.querySelector('.window>div').classList.toggle('show');
+		document.querySelector('.window span').classList.toggle('rotate');
+	});
+});
+//Food Section
+document.addEventListener("DOMContentLoaded", function(){
+	document.querySelector('.food>.accordion').addEventListener('click', function(){
+		document.querySelector('.food>div').classList.toggle('show');
+		document.querySelector('.food span').classList.toggle('rotate');
+	});
+});
+//Paint Section
+document.addEventListener("DOMContentLoaded", function(){
+	document.querySelector('.paint>.accordion').addEventListener('click', function(){
+		document.querySelector('.paint>div').classList.toggle('show');
+		document.querySelector('.paint span').classList.toggle('rotate');
+	});
+});
